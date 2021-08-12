06a2c94cfa1bc5574e0017bf2a7857b799a69ac3
Liam Parry <62780991+liam-parry@users.noreply.github.com>
2021-08-12 21:57:24 +0700

    upload

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    introTitle: 'introTitle',
+    introDesc: `introDesc`,
+    btnIntro: 'btnIntro',
+    title: 'title',
+    desc: 'desc',
+    btnYes: 'btnYes',
+    btnNo: 'btnNo',
+    question:'question',
+    btnReply: 'btnReply',
+    reply: 'reply',
+    mess: 'mess',
+    messDesc: 'messDesc',
+    btnAccept: 'btnAccept',
+    messLink: 'messLink', //link fb
+    bgLink: 'https://huypham9205.github.io/tang-crush/img/bg.webp', //link ảnh nền
+    iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
+    lookMeLink: 'https://huypham9205.github.io/tang-crush/img/lookMe.jpg', //link ảnh lookMe
+    soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
+    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+}
