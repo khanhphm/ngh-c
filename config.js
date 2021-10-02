9a2c19b4480c9e7009c74261ebc0b596ccc15a4a
thienlamlehoang374 <86696665+thienlamlehoang374@users.noreply.github.com>
2021-10-02 19:57:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,21 @@
 const CONFIG = {
-    introTitle: 'introTitle',
-    introDesc: `introDesc`,
-    btnIntro: 'btnIntro',
-    title: 'title',
-    desc: 'desc',
-    btnYes: 'btnYes',
-    btnNo: 'btnNo',
-    question:'question',
-    btnReply: 'btnReply',
-    reply: 'reply',
-    mess: 'mess',
-    messDesc: 'messDesc',
-    btnAccept: 'btnAccept',
-    messLink: 'messLink', //link fb
-    bgLink: 'https://huypham9205.github.io/tang-crush/img/bg.webp', //link ảnh nền
-    iputBgLink: 'https://huypham9205.github.io/tang-crush/img/iput-bg.jpg', //link iputBgLink
-    lookMeLink: 'https://huypham9205.github.io/tang-crush/img/lookMe.jpg', //link ảnh lookMe
+    introTitle: 'xin chao m!',
+    introDesc: `t có điều này muốn hỏi cậu, nhớ phải trả lời thật lòng nka`,
+    btnIntro: 'okele',
+    title: 'm sẽ cho t 200ka phải k?',
+    desc: 'm không trả lời hoặc thoát ra, tức là m đã đồng ý r đó nha :> ',
+    btnYes: 'cóooooooo<33333',
+    btnNo: 'khồng(t thách m bấm đc đó con tró)',
+    question:'m có chắc là đồng ý cho t k?',
+    btnReply: 'gửi cho lemmm <3',
+    reply: 't chắc chắn đồng ý mak, t sẽ hiến 200ka vì lem thân iu<3',
+    mess: 't biết mà 🥰. iu 😘😘',
+    messDesc: 'còn giờ thì ib fb chuyển khoản cho t:)).',
+    btnAccept: 'okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100044156002159' 
+    bgLink: 'https://znews-photo.zadn.vn/w660/Uploaded/aohvtsw/2019_09_28/o11_thumb.jpg', //link ảnh nền
+    iputBgLink: 'https://znews-photo.zadn.vn/w660/Uploaded/aohvtsw/2019_09_28/o11_thumb.jpg', //link iputBgLink
+    lookMeLink: 'https://i.pinimg.com/originals/15/a4/8e/15a48e0bac682fd51afc57a09f13df3e.jpg', //link ảnh lookMe
     soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
     duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
     swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
